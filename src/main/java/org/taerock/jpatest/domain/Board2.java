7d306cdacb940f12001245fd92425c12fd4a2409
LTH4823 <peter4823zx@naver.com>
2022-08-03 23:19:44 +0900

    design for FK

--- src/main/java/org/taerock/jpatest/domain/Board2.java
+++ src/main/java/org/taerock/jpatest/domain/Board2.java
@@ -1,9 +1,6 @@
 package org.taerock.jpatest.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Board2 {
@@ -11,5 +8,9 @@ public class Board2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
+
+    //한명의 작성자 기준으로 FK 설계시
+    @ManyToOne
+    private Member2 writer;
 
 }
